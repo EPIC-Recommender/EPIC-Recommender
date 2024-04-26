4d689187922eec03793813bcad04dafcbbe4629d
OmerCevruk <omer.cevrukk@gmail.com>
2024-04-27 02:12:48 +0300

    new abstract class for data holders

--- dataWriter.py
+++ dataWriter.py
@@ -1,4 +1,23 @@
-class writer:
+from typing import Optional, List, Union
+from abc import ABC, abstractmethod
+import psycopg2
+
+class DataHolder(ABC):
     def __init__(self) -> None:
         pass
+    
+    @abstractmethod
+    def write(db):
+        pass
 
+class MovieData:
+    """this class holds the scraped data of movies."""
+    def __init__(self):
+        pass
+
+class DataWriter():
+    """Uses Data classes to write to the data base."""
+    def __init__(self):
+        pass
+            
+        
