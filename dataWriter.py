e75748332cb7aabdfb9b0e3e03b3522b0c231e31
OmerCevruk <omer.cevrukk@gmail.com>
2024-04-27 11:12:16 +0300

    a

--- dataWriter.py
+++ dataWriter.py
@@ -1,116 +1,83 @@
 from typing import Optional, List, Union
-from abc import ABC, abstractmethod
 import psycopg2
 
-class DataHolder(ABC):
-    """This is an abstract class for data holders
-    
-    Args:
-        name(str) which many to many table the class should be connected to 
-        example "movie_actor"
-    """
-    def __init__(self,name = None) -> None:
-        self.name = name
-        pass
-    
-class GenreData(DataHolder):
-    """this class holds the scraped data of movies.
-       for example 
-    """
-    def __init__(self,genre=None):
-        super().__init__("movie_genre")
-        self.genre = genre
+class MovieDatabaseWriter:
+    def __init__(self, dbname: str, user: str, password: str, host: str = 'localhost', port: int = 5432) -> None:
+        self.conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        self.cur = self.conn.cursor()
 
-class MovieData(DataHolder):
-    """this class holds the scraped data of movies.
-       for example 
-    """
-    def __init__(self,
-                 title: str,
-                 imdb_rating: int,
-                 rotten_rating: int,
-                 meta_rating: int,
-                 pg_rating: str,
-                 ):
-        super().__init__()
-        self.title = title
-        self.imdb_rating = imdb_rating
-        self.rotten_rating = rotten_rating
-        self.meta_rating = meta_rating
-        self.pg_rating = pg_rating
+    def close_connection(self) -> None:
+        self.cur.close()
+        self.conn.close()
 
-class ActorData(DataHolder):
-    """this class holds the scraped data of movies.
-       for example 
-    """
-    def __init__(self,
-                 name: str,
-                 gender: bool,
-                 nationality: str,
-                 DOB: str
-                 ):
-        super().__init__("movie_actor")
-        self.name = name
-        self.gender = gender
-        self.nationality = nationality
-        self.DOB = DOB
-        
-class DirectorData(DataHolder):
-    """this class holds the scraped data of movies.
-       for example 
-    """
-    def __init__(self,
-                 name: str,
-                 gender: bool,
-                 nationality: str,
-                 DOB: str
-                 ):
-        super().__init__("movie_director")
-        self.name = name
-        self.gender = gender
-        self.nationality = nationality
-        self.DOB = DOB
+    def insert_movie(self, title: str, movei_awards: Optional[int] = None, imdb_rating: Optional[float] = None,
+                     rotten_rating: Optional[float] = None, meta_rating: Optional[float] = None, pg_rating: Optional[str] = None,
+                     genre: Optional[List[str]] = None) -> int:
+        sql = """INSERT INTO public.movie ("Title", movei_awards, imdb_rating, rotten_rating, meta_rating, pg_rating, genre)
+                 VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING "ID";"""
+        values = (title, movei_awards, imdb_rating, rotten_rating, meta_rating, pg_rating, genre)
+        self.cur.execute(sql, values)
+        movie_id_row = self.cur.fetchone()
+        if movie_id_row:
+            movie_id = movie_id_row[0]
+            self.conn.commit()
+            return movie_id
+        else:
+            print("insert movie no id")
+            return -1
 
-class ProducerData(DataHolder):
-    """this class holds the scraped data of movies.
-       for example 
-    """
-    def __init__(self,
-                 name: str,
-                 gender: bool,
-                 nationality: str,
-                 DOB: str
-                 ):
-        super().__init__("movie_producer")
-        self.name = name
-        self.gender = gender
-        self.nationality = nationality
-        self.DOB = DOB
+    def insert_person(self, name: str, gender: Optional[bool] = None, nationality: Optional[str] = None,
+                      dob: Optional[str] = None) -> None:
+        sql = """INSERT INTO public.person ("Name", gender, nationality, "DOB")
+                 VALUES (%s, %s, %s, %s)"""
+        values = (name, gender, nationality, dob)
+        self.cur.execute(sql, values)
+        self.conn.commit()
 
-class PersonAwardData(DataHolder):
-    """this class holds the scraped data of movies.
-       for example 
-    """
-    def __init__(self):
-        super().__init__("award_person") 
+    def insert_actor(self, name: str, gender: Optional[bool] = None, nationality: Optional[str] = None,
+                     dob: Optional[str] = None) -> None:
+        self.insert_person(name, gender, nationality, dob)
+        actor_id = self.cur.lastrowid  # Assuming "ID" is serial and auto-incremented
+        self.insert_movie_actor(movie_id=movie_id, actor_id=actor_id)
 
-class MovieAwardData(DataHolder):
-    """this class holds the scraped data of movies.
-       for example 
-    """
-    def __init__(self):
-        super().__init__("movie_award")
+    def insert_director(self, name: str, gender: Optional[bool] = None, nationality: Optional[str] = None,
+                      dob: Optional[str] = None) -> None:
+        self.insert_person(name, gender, nationality, dob)
+        director_id = self.cur.lastrowid  # Assuming "ID" is serial and auto-incremented
+        self.insert_movie_director(movie_id=movie_id, director_id=director_id)
 
-class SynopsisData(DataHolder):
-    """this class holds the scraped data of movies.
-       for example 
-    """
-    def __init__(self):
-        super().__init__("movie_producer")
+    def insert_movie_actor(self, movie_id: int, actor_id: int) -> None:
+        sql = """INSERT INTO public.movie_actor (movie, actor)
+                 VALUES (%s, %s)"""
+        values = (movie_id, actor_id)
+        self.cur.execute(sql, values)
+        self.conn.commit()
 
-class DataWriter():
-    """Uses Data classes to write to the data base."""
-    def __init__(self):
-        pass
-            
-        
+    def insert_movie_director(self, movie_id: int, director_id: int) -> None:
+        sql = """INSERT INTO public.movie_director (movie, director)
+                 VALUES (%s, %s)"""
+        values = (movie_id, director_id)
+        self.cur.execute(sql, values)
+        self.conn.commit()
+
+# Example usage:
+writer = MovieDatabaseWriter(dbname='your_db', user='your_user', password='your_password')
+
+# Inserting a movie and retrieving its ID
+movie_id = writer.insert_movie(title='The Shawshank Redemption', imdb_rating=9.3, genre=['Drama', 'Crime'])
+
+# Inserting an actor
+writer.insert_actor(name='Tim Robbins', dob='1958-10-16')
+
+# Inserting a director
+writer.insert_director(name='Frank Darabont', dob='1959-01-28')
+
+# Don't forget to close the connection when done
+writer.close_connection()
+       
