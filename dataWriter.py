ccdcbea78cd7c2303344d502447eddc8fc20e7e6
OmerCevruk <omer.cevrukk@gmail.com>
2024-04-27 19:51:35 +0300

    every insert method is created
    only bug fixing is left for the class

--- dataWriter.py
+++ dataWriter.py
@@ -31,34 +31,84 @@ class MovieDatabaseWriter:
                  VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING "ID";"""
         values = (title, movei_awards, imdb_rating, rotten_rating, meta_rating, pg_rating, genre)
         self.cur.execute(sql, values)
-        # movie_id_row = self.cur.fetchone()
-        # if movie_id_row:
-        #     movie_id = movie_id_row[0]
-        #     self.conn.commit()
-        #     self.current_movie_id = movie_id
-        #     return movie_id
-        # else:
-        #     print("insert movie no id")
-        #     return -1
-        self.current_movie_id = self.cur.lastrowid
+        self.current_movie_id = self.get_last_inserted_id()
+        self.conn.commit()
         return self.current_movie_id
 
     def insert_person(self, name: str, gender: Optional[bool] = None, nationality: Optional[str] = None,
-                      dob: Optional[str] = None) -> None:
+                      dob: Optional[str] = None) -> int:
 
         sql = """INSERT INTO public.person ("Name", gender, nationality, "DOB")
                  VALUES (%s, %s, %s, %s)"""
         values = (name, gender, nationality, dob)
         self.cur.execute(sql, values)
+        id = self.get_last_inserted_id()
         self.conn.commit()
+        return id
 
-#TODO make the parts after this line work
+
     def insert_director(self, name: str, gender: Optional[bool] = None, nationality: Optional[str] = None,
                       dob: Optional[str] = None) -> None:
 
-        self.insert_person(name, gender, nationality, dob)
-        director_id = self.cur.lastrowid  # Assuming "ID" is serial and auto-incremented
-        self.insert_movie_director(movie_id=movie_id, director_id=director_id)
+        director_id = self.insert_person(name, gender, nationality, dob)
+        try:
+            self.insert_movie_director(movie_id=self.current_movie_id, director_id=director_id)
+        except Exception:
+            print("error at insert_person make sure the movie_id and director_id correct")
+
+
+    def insert_actor(self, name: str, gender: Optional[bool] = None, nationality: Optional[str] = None,
+                    dob: Optional[str] = None) -> None:
+
+        actor_id = self.insert_person(name, gender, nationality, dob)
+        try:
+            self.insert_movie_actor(movie_id=self.current_movie_id, actor_id=actor_id)
+        except Exception:
+            print("error at insert_person make sure the movie_id and director_id correct")
+
+    def insert_award(self, name:str,date:str) -> int:
+        sql = """INSERT INTO public.award ("name","date")
+                 VALUES (%s,%s)"""
+        values = (name,date)
+        self.cur.execute(sql,values)
+        id = self.get_last_inserted_id()
+        self.conn.commit()
+        return id
+
+    def insert_genre(self, genre_name: str) -> int:
+        sql = """INSERT INTO public.genre (genre_name)
+                 VALUES (%s) RETURNING genre_id;"""
+        values = (genre_name,)
+        self.cur.execute(sql, values)
+        genre_id = self.get_last_inserted_id()
+        self.conn.commit()
+        self.insert_movie_genre(self.current_movie_id, genre_id)
+        return genre_id
+
+    def insert_synopsis(self,synopsis_text: str) -> int:
+        sql = """INSERT INTO public.synopsis (synopsis)
+                 VALUES (%s);"""
+        values = (synopsis_text,)
+        self.cur.execute(sql, values)
+        synopsis_id = self.get_last_inserted_id()
+        self.conn.commit()
+        self.insert_movie_synopsis(self.current_movie_id, synopsis_id)
+        return synopsis_id
+    
+#TODO add award inserts 
+    def insert_movie_synopsis(self, movie_id: int, synopsis_id: int) -> None:
+        sql = """INSERT INTO public.movie_synopsis (movie, synopsis)
+                VALUES (%s, %s)"""
+        values = (movie_id, synopsis_id)
+        self.cur.execute(sql, values)
+        self.conn.commit()
+
+    def insert_movie_genre(self, movie_id: int, genre_id: int) -> None:
+        sql = """INSERT INTO public.movie_genre (movie_id, genre_id)
+                 VALUES (%s, %s)"""
+        values = (movie_id, genre_id)
+        self.cur.execute(sql, values)
+        self.conn.commit()
 
     def insert_movie_actor(self, movie_id: int, actor_id: int) -> None:
 
@@ -68,12 +118,6 @@ class MovieDatabaseWriter:
         self.cur.execute(sql, values)
         self.conn.commit()
 
-    def insert_actor(self, name: str, gender: Optional[bool] = None, nationality: Optional[str] = None,
-                    dob: Optional[str] = None) -> None:
-        self.insert_person(name, gender, nationality, dob)
-        actor_id = self.cur.lastrowid  # Assuming "ID" is serial and auto-incremented
-        self.insert_movie_actor(movie_id=self.current_movie_id, actor_id=actor_id)
-
     def insert_movie_director(self, movie_id: int, director_id: int) -> None:
         sql = """INSERT INTO public.movie_director (movie, director)
                  VALUES (%s, %s)"""
@@ -81,18 +125,44 @@ class MovieDatabaseWriter:
         self.cur.execute(sql, values)
         self.conn.commit()
 
+    def insert_movie_award(self,movie_id:int, award_id:int):
+        sql = """INSERT INTO public.movie_award( "award_ID", "movie_ID")
+                 VALUES (%s,%s)"""
+        values = (award_id,movie_id)
+        self.cur.execute(sql,values)
+        self.conn.commit
+
+    def insert_person_award(self,person_id:int, award_id:int):
+        sql = """INSERT INTO public.award_person( "award_ID", "person_ID")
+                 VALUES (%s,%s)"""
+        values = (award_id,person_id)
+        self.cur.execute(sql,values)
+        self.conn.commit
+
+    def get_last_inserted_id(self) -> int:
+
+        self.cur.execute("SELECT LASTVAL();")
+        row = self.cur.fetchone()
+
+        if row:
+            return row[0]
+        else:
+            return -1
+if __name__ == "__main__":
 # Example usage:
-writer = MovieDatabaseWriter(dbname='EpicDB', user='postgres', password='')
-
+    writer = MovieDatabaseWriter(dbname='EpicDB', user='postgres', password='')
 # Inserting a movie and retrieving its ID
-writer.insert_movie(title='The Shawshank Redemption', imdb_rating=9)
-
+    movie_id = writer.insert_movie(title='The Shawshank Redemption', imdb_rating=9)
 # Inserting an actor
-# writer.insert_actor(name='Tim Robbins', dob='1958-10-16')
-
+    writer.insert_actor(name='Tim Robbins', dob='1958-10-16')
 # Inserting a director
-# writer.insert_director(name='Frank Darabont', dob='1959-01-28')
-
+    writer.insert_director(name='Frank Darabont', dob='1959-01-28')
+# Inserting a genre and associating it with the movie
+    genre_id = writer.insert_genre(genre_name='Drama')
+# Inserting a synopsis and associating it with the movie
+    synopsis_id = writer.insert_synopsis(synopsis_text='Two imprisoned men bond over a number of years, finding solace and common decency.')
+# Inserting an award
+    award_id = writer.insert_award(name='Oscar', date='1995-03-27')
 # Don't forget to close the connection when done
-# writer.close_connection()
-       
+    writer.close_connection()
+        
