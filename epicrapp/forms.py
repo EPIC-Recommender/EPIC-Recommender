b619fb20f185604bb86c88f7ba063457c763b24b
Burak Yilmaz <116060634+Laytenn@users.noreply.github.com>
2024-05-29 00:30:59 +0300

    UI Improvement
    May contain issue.

--- epicrapp/forms.py
+++ epicrapp/forms.py
@@ -8,12 +8,12 @@ class MovieForm(forms.ModelForm):
         model = Movie
         fields = ['title', 'release_year', 'imdb_rating', 'rotten_rating', 'meta_rating', 'pg_rating']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'release_year': forms.NumberInput(attrs={'class': 'form-control'}),
-            'imdb_rating': forms.NumberInput(attrs={'class': 'form-control'}),
-            'rotten_rating': forms.NumberInput(attrs={'class': 'form-control'}),
-            'meta_rating': forms.NumberInput(attrs={'class': 'form-control'}),
-            'pg_rating': forms.NumberInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control','autofocus':'false'}),
+            'release_year': forms.NumberInput(attrs={'class': 'form-control','autofocus':'false'}),
+            'imdb_rating': forms.NumberInput(attrs={'class': 'form-control','autofocus':'false'}),
+            'rotten_rating': forms.NumberInput(attrs={'class': 'form-control','autofocus':'false'}),
+            'meta_rating': forms.NumberInput(attrs={'class': 'form-control','autofocus':'false'}),
+            'pg_rating': forms.NumberInput(attrs={'class': 'form-control','autofocus':'false'}),
         }
 
 
